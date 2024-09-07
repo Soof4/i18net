f8920ef55ca7a5f48a1d1e9955b2e01738513651
Soofa <emr3.cbc@gmail.com>
2024-09-07 18:20:15 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# Publish for Linux
 dotnet publish -c Release -r linux-x64 --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true -o ./publish/linux
+
+# Rename Linux executable
+mv ./publish/linux/i18net ./publish/linux/i18net-linux
+
+# Publish for Windows
 dotnet publish -c Release -r win-x64 --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true -o ./publish/windows
+
+# Rename Windows executable
+mv ./publish/windows/i18net.exe ./publish/windows/i18net-win.exe
+
+# Publish for macOS
 dotnet publish -c Release -r osx-x64 --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true -o ./publish/macos
+
+# Rename macOS executable
+mv ./publish/macos/i18net ./publish/macos/i18net-mac
 
